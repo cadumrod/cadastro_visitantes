cfcffe6663bd9501bf0b31ffbaa691f13234af18
cadumrod <cadu.mrod@gmail.com>
2024-02-22 21:20:07 -0300

    Atualizando a versão python para 12

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\cadum\documents\Projeto cadastro de visitantes\venv
+set VIRTUAL_ENV=C:\Users\T-Gamer\desktop\Projects GitHub\Projeto cadastro visitantes\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
