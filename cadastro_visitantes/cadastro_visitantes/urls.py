7a584a1aa3645f59ab6304b8808339275de13af6
Carlos Rodrigues <cadu.mrod@gmail.com>
2024-02-26 17:09:25 -0300

    atualizando urls

--- cadastro_visitantes/cadastro_visitantes/urls.py
+++ cadastro_visitantes/cadastro_visitantes/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.visitantes, name='visitantes'),
     path('cadastro/', views.criar, name='novo_visitante'),
-    path('cadastro/<int:id_visitante>', views.editar, name='editar'),
-    path ('/<int:id_visitante>', views.detalhe, name='detalhe')
+    path('editar/<int:id_visitante>', views.editar, name='editar'),
+    path ('detalhes/<int:id_visitante>', views.detalhe, name='detalhe')
 ]
