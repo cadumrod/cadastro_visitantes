6f0d412a62b0ecba8b297b062c906dc7844ad2bb
Carlos Rodrigues <cadu.mrod@gmail.com>
2024-02-29 16:24:50 -0300

    atualizando style

--- cadastro_visitantes/cadastro_visitantes/settings.py
+++ cadastro_visitantes/cadastro_visitantes/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cadastro_app',
+    'crispy_forms',
+    'crispy_bootstrap5',
+    'widget_tweaks'
 ]
 
 MIDDLEWARE = [
@@ -116,7 +119,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
