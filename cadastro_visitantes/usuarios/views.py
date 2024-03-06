e3427ce628cbea6bd6c5b6ca472cafe5efcce780
Carlos Rodrigues <cadu.mrod@gmail.com>
2024-03-06 12:44:33 -0300

    atualizando cadastro de usuario no aplicativo

--- cadastro_visitantes/usuarios/views.py
+++ cadastro_visitantes/usuarios/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 def novo_usuario(request):
-    formulario = UserCreationForm()
+    if request.method == 'POST':
+        formulario = UserCreationForm(request.POST)
+        if formulario.is_valid():
+            formulario.save()
+            usuario = formulario.cleaned_data.get('username')
+            messages.success(request, f'O usuário {usuario} foi criado com sucesso!')
+            return redirect('visitantes')
+    else:
+        formulario = UserCreationForm()
+    
     return render(request, 'usuarios/registrar.html', {'formulario':formulario})
